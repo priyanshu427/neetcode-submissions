8c42479a7ca1818a759e1c63c14fe6d4e1fbab80
Priyanshu_Sharma <ps632977@gmail.com>
2026-07-21 15:13:32 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset=set()
+
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)    
+        return False
+
+# In Python, a set IS a Hash Set. in a normal for python to look up a certain number it will go sequentially. 
+# when a set get a element its run through a hash function. the element is converted memory address and is stored there.
+# when we check for the same element python converts it into hash and looks straight for that memory address and checks if the element is there.
+
